cd7c33c613b06cb7226451f637e4d11fe8b73038
Echo <since92x@gmail.com>
2017-01-13 10:56:42 +0800

    some scripts

---
+++ bin/initmachine.sh
@@ -0,0 +1,28 @@
+sudo apt-get install linux-headers-generic build-essential dkms
+sudo apt-get install --reinstall bcmwl-kernel-source 
+sudo apt-get install linux-source 
+sudo add-apt-repository ppa:noobslab/themes
+sudo add-apt-repository ppa:noobslab/icons
+sudo apt-get update
+sudo apt-get install unity-tweak-tool fonts-wqy-microhei flatabulous-theme ultra-flat-icons git docker screen ibus-pinyin language-pack-zh-hans
+sudo apt-get remove vim-common
+sudo apt-get install vim-common
+sudo apt-get install vim-gnome
+sudo apt install  vim-gnome
+#tar -zxvf flash_player_npapi_linux.x86_64.tar.gz 
+#sudo cp libflashplayer.so /usr/lib/firefox-addons/plugins/
+#sudo cp -r usr/* /usr/
+sudo apt-get install apt-transport-https ca-certificates
+sudo apt-key adv                --keyserver hkp://ha.pool.sks-keyservers.net:80                --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+cat  /etc/apt/sources.list.d/docker.list
+sudo apt-get update 
+apt-cache policy docker-engine
+sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt-get install docker-engine
+#sudo dpkg -i nvidia-docker_1.0.0.rc.3-1_amd64.deb 
+sudo groupadd docker
+sudo gpasswd -a ${USER} docker
+sudo service docker restart
+sudo usermod -aG docker $USER
+sudo service docker restart
